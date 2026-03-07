7bf5558ff3a53504e5d2c33a7304380d96145c10
Yi Lee <ylee1228@umd.edu>
2026-03-07 15:52:05 -0500

    Fix hw2 demo mismatch

--- RocqDemo.v
+++ RocqDemo.v
@@ -2,8 +2,8 @@ Require Import SQIR.NDSem.
 
 Local Open Scope ucom_scope.
 
-Definition left_circuit : base_ucom 2 := H 0 ; CNOT 0 1 ; H 0.
-Definition right_circuit : base_ucom 2 := H 1 ; CNOT 1 0 ; H 1.
+Definition left_circuit : base_ucom 2 := H 1 ; CNOT 1 0 ; H 1.
+Definition right_circuit : base_ucom 2 := H 0 ; CNOT 0 1 ; H 0.
 
 Lemma hw2_problem1_part2 :
   left_circuit ≡ right_circuit.
